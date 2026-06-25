598f28fee7cd5073da4a83460d4d0cb8115e2344
DCorleone <DCorleone@users.noreply.github.com>
2026-06-25 09:41:55 +0200

    Refactor run.sh to use bashio and update paths

--- teddycloud/rootfs/usr/local/bin/run.sh
+++ teddycloud/rootfs/usr/local/bin/run.sh
@@ -1,23 +1,15 @@
-#!/bin/bash
-set -e
+#!/usr/bin/with-contenv bashio
 
-# Erstelle Zielverzeichnisse in HA-gemappten Pfaden
-mkdir -p /config/teddycloud/certs
+# Erstelle Verzeichnisse in HA-gemappten Pfaden
+mkdir -p /config/teddycloud/certs/client
+mkdir -p /config/teddycloud/certs/server
 mkdir -p /config/teddycloud/config
 mkdir -p /data/teddycloud/content
 mkdir -p /data/teddycloud/library
 mkdir -p /data/teddycloud/firmware
 mkdir -p /data/teddycloud/cache
-mkdir -p /data/teddycloud/www/custom_img
+mkdir -p /data/teddycloud/custom_img
+mkdir -p /data/teddycloud/plugins
 
-# Lösche die vom Image deklarierten Volumes und ersetze durch Symlinks
-rm -rf /teddycloud/certs && ln -sf /config/teddycloud/certs /teddycloud/certs
-rm -rf /teddycloud/config && ln -sf /config/teddycloud/config /teddycloud/config
-rm -rf /teddycloud/data/content && ln -sf /data/teddycloud/content /teddycloud/data/content
-rm -rf /teddycloud/data/library && ln -sf /data/teddycloud/library /teddycloud/data/library
-rm -rf /teddycloud/data/firmware && ln -sf /data/teddycloud/firmware /teddycloud/data/firmware
-rm -rf /teddycloud/data/cache && ln -sf /data/teddycloud/cache /teddycloud/data/cache
-rm -rf /teddycloud/data/www/custom_img && ln -sf /data/teddycloud/www/custom_img /teddycloud/data/www/custom_img
-
-# Starte TeddyCloud
-exec /docker-entrypoint.sh
+bashio::log.info "Starting TeddyCloud..."
+exec /usr/local/bin/teddycloud
